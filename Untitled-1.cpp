1f06de60f6039d001c3e60a8f3b1a1a9333d660a
202205 <fijkui@163.com>
2023-08-07 14:09:20 +0800

    first commit

---
+++ Untitled-1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+void dfs(int k)
+{
+    cout<<k;
+}
+int main()
+{
+    cout<<"hello world\n";
+    int n;
+    scanf("%d",&n);
+    printf("%d\n",n);
+    dfs(n);
+}
---
+++ import time.py
@@ -0,0 +1,3 @@
+import math
+n=input(int())
+print(pow(2,n)-1)
